71bc4ef7bbc2d4926d000f3b5cbba0f83aefc226
Maximiliano Techera <maxi.techerag@gmail.com>
2020-11-11 13:18:19 -0300

    LastRev Content Validator (#6)
    * Add initial commit for LastRev cotnent validator
    
    * Remove private flag
    
    * Publish
    
     - @last-rev/content-validator@0.0.2-alpha.0

---
+++ packages/lastrev-content-validator/src/withContentValidation/getContent.ts
@@ -0,0 +1,41 @@
+export interface ContentValidationProps {
+  _id: string;
+}
+export const getContent = (type: string) => {
+  switch (type) {
+    case 'string':
+      return 'MISSING TEXT';
+    case 'boolean':
+      return 'MISSING TEXT';
+    default:
+      return 'MISSING VALUE';
+  }
+};
+interface ParsedPropTypes {
+  required: boolean;
+  type: {
+    name: string;
+  };
+}
+export const fillRequiredProps = <P extends ContentValidationProps>({
+  props,
+  propTypes
+}: {
+  props: P;
+  propTypes: ParsedPropTypes;
+}) => {
+  console.log('Component', { props, propTypes });
+  const newProps = { ...props };
+  Object.keys(propTypes).forEach((key) => {
+    if (!propTypes[key]) return;
+    const {
+      required,
+      type: { name }
+    } = propTypes[key];
+    if (required) {
+      newProps[key] = newProps[key] ?? getContent(name);
+    }
+  });
+  console.log('Component', { props, propTypes, newProps });
+  return newProps;
+};
