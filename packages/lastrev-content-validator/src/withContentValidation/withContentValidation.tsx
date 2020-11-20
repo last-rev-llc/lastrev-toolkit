c14689153415720cf1ac8250f20e4a9f997473f8
Max Techera <maxi.techerag@gmail.com>
2020-11-20 15:00:53 -0300

    Add Yup for debugging

--- packages/lastrev-content-validator/src/withContentValidation/withContentValidation.tsx
+++ packages/lastrev-content-validator/src/withContentValidation/withContentValidation.tsx
@@ -3,16 +3,28 @@ import parsePropTypes from 'parse-prop-types';
 import { ValidationContext } from './ContextValidationProvider';
 import { ContentValidationProps, fillRequiredProps, checkPropTypes } from './getContent';
 import { uniqueId } from 'lodash';
+import * as yup from 'yup';
 
-export const withContentValidation = ({ logLevel }: { logLevel?: 'ERROR' | 'DEBUG' } = {}) => <
-  P extends ContentValidationProps
->(
+interface Args {
+  logLevel?: 'ERROR' | 'DEBUG';
+  schema: yup.ObjectSchema;
+}
+
+export const withContentValidation = ({ logLevel, schema }: Args) => <P extends ContentValidationProps>(
   WrappedComponent: React.FunctionComponent<P>
 ): React.FC<P & ContentValidationProps> => (props: P & ContentValidationProps) => {
   const [id] = React.useState(uniqueId());
   const { handleError = () => {} } = React.useContext(ValidationContext);
   const propTypes = React.useMemo(() => parsePropTypes(WrappedComponent), []);
   const errors = React.useMemo(() => checkPropTypes({ propTypes, props }), [props]);
+  const schemaErrors = React.useMemo(() => {
+    try {
+      schema.validateSync(props);
+    } catch (errors) {
+      console.log('Errors');
+      return errors;
+    }
+  }, [props, schema]);
   React.useEffect(() => {
     if (errors) {
       handleError({
