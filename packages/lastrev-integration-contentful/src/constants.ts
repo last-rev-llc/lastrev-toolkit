093b37198af294c28874d6588caf2bdace9f9f44
Adam Harris <adam@lastrev.com>
2020-12-23 12:33:21 -0800

    added locale to write content json

--- packages/lastrev-integration-contentful/src/constants.ts
+++ packages/lastrev-integration-contentful/src/constants.ts
@@ -2,3 +2,4 @@ export const DEFAULT_SETTINGS_CONTENT_TYPE = 'settingsGlobal';
 export const DEFAULT_LOCALIZATION_LOOKUP_FIELD_NAME = 'localizationLookup';
 export const DEFAULT_SLUG_FIELD_NAME = 'slug';
 export const DEFAULT_ORDER_PARAM = 'sys.createdAt';
+export const DEFAULT_LOCALE_PARAM = 'en-US';
