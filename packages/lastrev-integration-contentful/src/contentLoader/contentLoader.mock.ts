a8293cc6740559b81140f577eea368e1f8bc1270
Max Techera <maxi.techerag@gmail.com>
2021-04-07 21:37:37 -0300

    Publish
     - @last-rev/adapter-contentful@4.3.0-alpha.0
     - @last-rev/build-prefetch-content@5.4.0-alpha.0
     - @last-rev/integration-contentful@5.3.0-alpha.0

---
+++ packages/lastrev-integration-contentful/src/contentLoader/contentLoader.mock.ts
@@ -0,0 +1,129 @@
+export default {
+  _id: '6smvyFzki47wtxRiQoCRdT',
+  _contentTypeId: 'pageAccelerator',
+  _href: '/accelerators/abn-amro-techstars-future-of-finance-accelerator',
+  _as: '/accelerators/[...slug]',
+  _modifiedDate: '2021-03-31T01:00:59.265Z',
+  slug: 'abn-amro-techstars-future-of-finance-accelerator',
+  sfdcID: 'a0P2S00000zzN2ZUAU',
+  programStatus: 'Accelerator in Progress',
+  categoryLocation: [
+    {
+      _id: '5BQ3RYi6pErXKyhFnWElm0',
+      _contentTypeId: 'categoryLocation',
+      _modifiedDate: '2021-03-26T21:41:41.274Z',
+      slug: 'amsterdam-netherlands',
+      name: 'Amsterdam, Netherlands',
+      hashtagValue: '#Amsterdam, Netherlands'
+    }
+  ],
+  categoryIndustry: [
+    {
+      _id: '7fG5ssodcoyDf40LqxD3cT',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:43.429Z',
+      slug: 'agtech-sustainability',
+      name: 'AgTech / Sustainability',
+      hashtagValue: '#AgTech / Sustainability'
+    },
+    {
+      _id: '5UUZq9vDqqoLVLPLA1nJVP',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:46.434Z',
+      slug: 'finance-banking',
+      name: 'Finance / Banking',
+      hashtagValue: '#Finance / Banking'
+    },
+    {
+      _id: '5EcdlCfmafUKkVdL6G8RcH',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:48.154Z',
+      slug: 'insurtech',
+      name: 'Insurtech',
+      hashtagValue: '#Insurtech'
+    },
+    {
+      _id: '5e9E7WyxQpvtnpLPgPaqDM',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:49.364Z',
+      slug: 'saas-mobile',
+      name: 'SaaS / Mobile',
+      hashtagValue: '#SaaS / Mobile'
+    },
+    {
+      _id: '5AW3F3oOn0wiUcnszpcpt4',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:51.764Z',
+      slug: 'marketing',
+      name: 'Marketing',
+      hashtagValue: '#Marketing'
+    },
+    {
+      _id: '6V9etBrmR4uDP5sgGMjZD1',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:53.095Z',
+      slug: 'proptech',
+      name: 'Proptech',
+      hashtagValue: '#Proptech'
+    },
+    {
+      _id: '79kb6lcmBxcD5vlNIowRRA',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:56.104Z',
+      slug: 'smart-cities',
+      name: 'Smart Cities',
+      hashtagValue: '#Smart Cities'
+    },
+    {
+      _id: '7Bztntgxx4diOATHT8GFjE',
+      _contentTypeId: 'categoryIndustry',
+      _modifiedDate: '2021-03-26T21:41:59.140Z',
+      slug: 'social-impact',
+      name: 'Social & Impact',
+      hashtagValue: '#Social & Impact'
+    }
+  ],
+  categoryAccelerator: {
+    _id: '6KbC57vSfvPbsXZkXQYp93',
+    _contentTypeId: 'categoryAccelerator',
+    _href: '/the-line/abn-amro-techstars-future-of-finance-accelerator',
+    _as: '/the-line/[...slug]',
+    _modifiedDate: '2021-03-26T21:41:39.997Z',
+    slug: 'abn-amro-techstars-future-of-finance-accelerator',
+    name: 'ABN AMRO & Techstars Future of Finance Accelerator',
+    hashtagValue: '#ABN AMRO & Techstars Future of Finance Accelerator'
+  },
+  dates: [
+    { date: '2021-06-17', dateDescriptionKey: 'demo_day', dateDescriptionLabel: 'Demo Day' },
+    {
+      date: '2021-03-29',
+      dateDescriptionKey: 'start_date',
+      dateDescriptionLabel: 'Accelerator Starts'
+    },
+    {
+      date: '2020-09-21',
+      dateDescriptionKey: 'applications_open_date',
+      dateDescriptionLabel: 'Applications Open'
+    },
+    {
+      date: '2020-12-20',
+      dateDescriptionKey: 'final_deadline',
+      dateDescriptionLabel: 'Final Deadline'
+    }
+  ],
+  selectableModules: [
+    {
+      _id: '3fQ60UmxMnQnNLezKLr5AK',
+      _contentTypeId: 'cardCollection',
+      _modifiedDate: '2021-03-31T01:00:35.106Z',
+      headerText: 'Companies',
+      cardStyle: 'Thumbnail',
+      logoURLs: [
+        {
+          logoURL:
+            'http://s3.amazonaws.com/ts-accel-connect-uploads/images/image_files/605b14f0c97c0a0007ed57c4/original/Valyuu_200x200.jpg'
+        }
+      ]
+    }
+  ]
+};
