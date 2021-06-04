8fd1ec51038a633a2a36de1944c3aab8d67b4559
Max Techera <maxi.techerag@gmail.com>
2021-06-04 12:25:49 -0300

    REmove debounce

--- packages/lastrev-content-validator/src/withContentValidation/ContextValidationProvider.tsx
+++ packages/lastrev-content-validator/src/withContentValidation/ContextValidationProvider.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import debounce from 'lodash/debounce';
+
 interface ErrorInstance {
   contentId: string;
   errors: {
@@ -25,30 +25,24 @@ export const ContentValidationProvider = ({ children, logLevel = 'DEBUG' }) => {
   const errorsById = React.useMemo(() => errors.reduce((accum, error) => ({ accum, [error.id]: error }), {}), [
     errors.join('')
   ]);
-  const handleErrors = React.useCallback(
-    debounce((errorsById) => {
-      const errorMarkers = document.querySelectorAll('[data-csk-error=true]');
-      errorMarkers.forEach((marker: HTMLElement) => {
-        const el = marker.nextElementSibling as HTMLElement;
-        const id = marker.dataset.cskErrorId;
-        const error = errorsById[id];
-        if (error) {
-          el.dataset.cskErrorId = id;
-          el.dataset.cskError = JSON.stringify(error);
-        }
-      });
-      switch (logLevel) {
-        case 'DEBUG':
-          console.log('ContentErrors', errors);
-          break;
-        default:
-          throw new Error(JSON.stringify(errors));
-      }
-    }, 300),
-    []
-  );
   React.useEffect(() => {
-    handleErrors(errorsById);
+    const errorMarkers = document.querySelectorAll('[data-csk-error=true]');
+    errorMarkers.forEach((marker: HTMLElement) => {
+      const el = marker.nextElementSibling as HTMLElement;
+      const id = marker.dataset.cskErrorId;
+      const error = errorsById[id];
+      if (error) {
+        el.dataset.cskErrorId = id;
+        el.dataset.cskError = JSON.stringify(error);
+      }
+    });
+    switch (logLevel) {
+      case 'DEBUG':
+        console.log('ContentErrors', errors);
+        break;
+      default:
+        throw new Error(JSON.stringify(errors));
+    }
   }, [errorsById]);
 
   const handleError = ({ contentId, errors, componentName, id, logLevel: instanceLogLevel }: ErrorInstance) => {
